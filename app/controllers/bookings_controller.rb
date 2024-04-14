ac040ca27443318f948ababd86cb834c2bb7a9ea
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-04-13 23:46:13 -0600

    Handle Booking creation with nested passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,8 +6,19 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
