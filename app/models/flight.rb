0499f4b6a2b2e2ec27c4dab6189c9f44baf4d9ca
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-04-13 20:52:38 -0600

    Add flight selection form

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,5 +3,9 @@ class Flight < ApplicationRecord
   validates :duration, comparison: { greater_than: 0 }
 
   belongs_to :departure_airport, class_name: "Airport", foreign_key: :departure_airport_id 
-  belongs_to :arrival_airport, class_name: "Airport", foreign_key: :arrival_airport_id 
+  belongs_to :arrival_airport, class_name: "Airport", foreign_key: :arrival_airport_id
+
+  def start_time
+    self.start.strftime("%H:%M %Z")
+  end
 end
