e4f4e9ddf410b26ce14b227db4fb063b0a9d5d75
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-04-13 20:33:16 -0600

    Add flight controller & search view

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,17 @@
+class FlightsController < ApplicationController
+  def index
+    @flights = Flight.where(departure_airport_id: params[:departure_airport_id].to_i)
+    .where(arrival_airport_id: params[:arrival_airport_id].to_i)
+    .where("date_trunc('day', start) = ?", params[:start])
+    # @searched = params.has_key?(:start)
+    @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
+    @date_options = Flight.select("date_trunc('day', start) AS start_date").distinct.order(:start_date)
+      .map { |flight| [format_date(flight.start_date), flight.start_date]}
+  end
+
+  private
+
+  def format_date(date)
+    date.strftime("%Y / %B / %d")
+  end
+end
